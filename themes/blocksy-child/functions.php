a644ebdb83baa0b45e3da688beb8fb01dff84534
Maxime Le Louarn <maxime.lelouarn@gmail.com>
2024-03-01 20:08:03 +0100

    Pages1&2cssformV1

--- themes/blocksy-child/functions.php
+++ themes/blocksy-child/functions.php
@@ -4,4 +4,5 @@ if (! defined('WP_DEBUG')) {
 }
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 });
