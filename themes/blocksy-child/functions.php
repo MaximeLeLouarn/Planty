7e59c8a33d0be529309d26902fbd286ba60f67ab
Maxime Le Louarn <maxime.lelouarn@gmail.com>
2024-03-25 23:49:28 +0100

    End of project

--- themes/blocksy-child/functions.php
+++ themes/blocksy-child/functions.php
@@ -10,16 +10,6 @@ add_action( 'wp_enqueue_scripts', function () {
 // Removing <p> from popping up in cf7
 add_filter('wpcf7_autop_or_not', '__return_false');
 
-// Add admin item in the menu
-// add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
-// function add_extra_item_to_nav_menu( $items, $args ) {}
-
-
-// function add_admin_menu() {
-// 	add_menu_page('admin','Admin', 'manage_options', 'Admin', 'add_admin_menu_main', 2);
-// }
-// add_action('admin_menu','add_admin_menu');
-// function add_admin_menu_main(){}
 
 add_filter( 'wp_nav_menu_items', 'prefix_add_menu_item', 10, 2 );
 /**
